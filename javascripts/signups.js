d07fde36c749ea4f7d2d5dbe680799a0d7233181
54409320 <aghio@live.com>
2016-07-03 00:45:02 -0400

    changed background, changed from hover to click

--- javascripts/signups.js
+++ javascripts/signups.js
@@ -1,145 +1,87 @@
-/*$("document").ready(function(){
-    $(".box_hx").hover(
-    function () {
-        $("#logo").fadeOut("10");
-        $("#desc_hx").fadeIn("500");
-    },
-    function () {
-        $("#desc_hx").fadeOut("10");
-        $("#logo").fadeIn("500");
-    });
-});*/
+function clearDescs(){
+    $("#logo").css("display","none");
+    $("#desc_hx").css("display","none");
+    $("#desc_jr").css("display","none");
+    $("#desc_sr").css("display","none");
+    $("#desc_mr").css("display","none");
+    $("#desc_vr").css("display","none");
+    $("#desc_we").css("display","none");
+    $("#desc_de").css("display","none");
+    $("#desc_fi").css("display","none");
+    $("#desc_lo").css("display","none");
+    $("#desc_pr").css("display","none");
+    $(".box_hx").removeClass("selected");
+    $(".box_jr").removeClass("selected");
+    $(".box_sr").removeClass("selected");
+    $(".box_mr").removeClass("selected");
+    $(".box_vr").removeClass("selected");
+    $(".box_we").removeClass("selected");
+    $(".box_de").removeClass("selected");
+    $(".box_lo").removeClass("selected");
+    $(".box_fi").removeClass("selected");
+    $(".box_pr").removeClass("selected");
+} 
 
-/*$("document").ready(function(){
-    $(".box_hx").hover(
-    function () {
-        $("#logo").css("visibility","hidden");
-        $("#desc_hx").css("visibility","visible");
-    },
-    function () {
-        $("#desc_hx").css("visibility","hidden");
-        $("#logo").css("visibility","visible");
-    });
-});*/
 
 $("document").ready(function(){
-    $(".box_hx").hover(
+    $(".box_hx").click(
     function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
+        clearDescs();
         $("#desc_hx").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_hx").css("display","none");
-        $("#logo").css("display","block");
+        $(".box_hx").addClass("selected");
     });
-    
-    $(".box_jr").hover(
+    $(".box_fi").click(
     function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_jr").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_jr").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_sr").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_sr").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_sr").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_mr").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_mr").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_mr").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_vr").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_vr").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_vr").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_de").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_de").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_de").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_fi").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
+        clearDescs();
         $("#desc_fi").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_fi").css("display","none");
-        $("#logo").css("display","block");
+        $(".box_fi").addClass("selected")
     });
-    
-    $(".box_lo").hover(
+    $(".box_we").click(
     function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_lo").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_lo").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_pr").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
-        $("#desc_pr").css("display","block");
-    },
-    function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_pr").css("display","none");
-        $("#logo").css("display","block");
-    });
-    
-    $(".box_we").hover(
-    function () {
-        $('.signupbtn').css("display","none");
-        $("#logo").css("display","none");
+        clearDescs();
         $("#desc_we").css("display","block");
-    },
+        $(".box_we").addClass("selected")
+    });
+    $(".box_de").click(
     function () {
-        $('.signupbtn').css("display","inline-flex");
-        $("#desc_we").css("display","none");
-        $("#logo").css("display","block");
+        clearDescs();
+        $("#desc_de").css("display","block");
+        $(".box_de").addClass("selected")
+    });
+    $(".box_jr").click(
+    function () {
+        clearDescs();
+        $("#desc_jr").css("display","block");
+        $(".box_jr").addClass("selected")
+    });
+    $(".box_mr").click(
+    function () {
+        clearDescs();
+        $("#desc_mr").css("display","block");
+        $(".box_mr").addClass("selected")
+    });
+    $(".box_sr").click(
+    function () {
+        clearDescs();
+        $("#desc_sr").css("display","block");
+        $(".box_sr").addClass("selected")
+    });
+    $(".box_vr").click(
+    function () {
+        clearDescs();
+        $("#desc_vr").css("display","block");
+        $(".box_vr").addClass("selected")
+    });
+    $(".box_lo").click(
+    function () {
+        clearDescs();
+        $("#desc_lo").css("display","block");
+        $(".box_lo").addClass("selected")
+    });
+    $(".box_pr").click(
+    function () {
+        clearDescs();
+        $("#desc_pr").css("display","block");
+        $(".box_pr").addClass("selected")
     });
 });
