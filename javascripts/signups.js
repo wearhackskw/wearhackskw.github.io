fa8a8421ef8fc1dc6ecfb3983b416122e2b5316d
54409320 <aghio@live.com>
2016-07-05 00:00:37 -0400

    done for tonight

--- javascripts/signups.js
+++ javascripts/signups.js
@@ -5,8 +5,6 @@ function clearDescs(){
     $("#desc_sr").css("display","none");
     $("#desc_mr").css("display","none");
     $("#desc_vr").css("display","none");
-    $("#desc_we").css("display","none");
-    $("#desc_de").css("display","none");
     $("#desc_fi").css("display","none");
     $("#desc_lo").css("display","none");
     $("#desc_pr").css("display","none");
@@ -15,12 +13,11 @@ function clearDescs(){
     $(".box_sr").removeClass("selected");
     $(".box_mr").removeClass("selected");
     $(".box_vr").removeClass("selected");
-    $(".box_we").removeClass("selected");
-    $(".box_de").removeClass("selected");
     $(".box_lo").removeClass("selected");
     $(".box_fi").removeClass("selected");
     $(".box_pr").removeClass("selected");
 } 
+
 
 $("document").ready(function(){
     $(".box_hx").click(
@@ -35,20 +32,6 @@ $("document").ready(function(){
         clearDescs();
         $("#desc_fi").css("display","block");
         $(".box_fi").addClass("selected")
-        $(".signupbtn").removeClass("unselectedbtn");
-    });
-    $(".box_we").click(
-    function () {
-        clearDescs();
-        $("#desc_we").css("display","block");
-        $(".box_we").addClass("selected")
-        $(".signupbtn").removeClass("unselectedbtn");
-    });
-    $(".box_de").click(
-    function () {
-        clearDescs();
-        $("#desc_de").css("display","block");
-        $(".box_de").addClass("selected")
         $(".signupbtn").removeClass("unselectedbtn");
     });
     $(".box_jr").click(
